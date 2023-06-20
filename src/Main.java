7260a9984d3e162897a222256a81d165ea0a9c31
tiep204 <dangtrantiep240604@gmail.com>
2023-06-20 20:11:01 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import ra.MyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("______________________________");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+}
